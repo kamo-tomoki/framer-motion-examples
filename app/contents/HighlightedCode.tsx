d3e8b11ac8963f7677ce560df6687a7bac91bf8c
Kamo Tomoki <tomokiti919@gmail.com>
2024-01-07 19:37:46 +0900

    Adding code-copy feature

--- app/contents/HighlightedCode.tsx
+++ app/contents/HighlightedCode.tsx
@@ -2,9 +2,11 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import "./HighlightedCode.css";
 import { useState } from "react";
-import { Tabs, Tab } from "@nextui-org/react";
+import { Tabs, Tab, Button } from "@nextui-org/react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import ReactIcon from "../icons/ReactIcon";
 import CssIcon from "../icons/CssIcon";
+import CopyIcon from "../icons/CopyIcon";
 
 type Props = {
   sampleCode: { [key: string]: string };
@@ -14,30 +16,36 @@ const HighlightedCode: React.FC<Props> = ({ sampleCode }) => {
 
   return (
     <div className="code-container">
-      <Tabs
-        className="dark"
-        onSelectionChange={(key) => {
-          typeof key === "string" && setActiveTab(key);
-        }}
-      >
-        {Object.keys(sampleCode).map((key) => (
-          <Tab
-            key={key}
-            title={
-              <div className="flex items-center space-x-2">
-                {key.split(".").pop() === "tsx" ? (
-                  <ReactIcon />
-                ) : key.split(".").pop() === "css" ? (
-                  <CssIcon />
-                ) : (
-                  <></>
-                )}
-                <span>{key}</span>
-              </div>
-            }
-          />
-        ))}
-      </Tabs>
+      <div className="code-header dark">
+        <Tabs
+          onSelectionChange={(key) => {
+            typeof key === "string" && setActiveTab(key);
+          }}
+        >
+          {Object.keys(sampleCode).map((key) => (
+            <Tab
+              key={key}
+              title={
+                <div className="flex items-center space-x-2">
+                  {key.split(".").pop() === "tsx" ? (
+                    <ReactIcon />
+                  ) : key.split(".").pop() === "css" ? (
+                    <CssIcon />
+                  ) : (
+                    <></>
+                  )}
+                  <span>{key}</span>
+                </div>
+              }
+            />
+          ))}
+        </Tabs>
+        <CopyToClipboard text={sampleCode[activeTab]}>
+          <Button isIconOnly>
+            <CopyIcon />
+          </Button>
+        </CopyToClipboard>
+      </div>
       <SyntaxHighlighter
         language={activeTab.split(".").pop()}
         style={vscDarkPlus}
