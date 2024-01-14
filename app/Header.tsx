c44efacc42189f7602d64c05a68159dcde91cf09
Kamo Tomoki <tomokiti919@gmail.com>
2024-01-14 14:42:27 +0900

    Update header style

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,10 +1,26 @@
 const Header = () => {
   return (
     <header>
-      <div>
-        <h1 className="font-bold text-2xl">
+      <div className="header-container">
+        <h1>
           <span>Framer Motion</span> Samples
         </h1>
+        <div className="flex mr-5 font-bold">
+          <a
+            className="link mr-8"
+            href="https://www.framer.com/motion/"
+            target="_blank"
+          >
+            Official Site
+          </a>
+          <a
+            className="link"
+            href="https://github.com/kamo-tomoki/framer-motion-samples"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
     </header>
   );
